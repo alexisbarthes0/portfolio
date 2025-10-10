06e6188e5d9547b0611ac894d94cbe066c2d6de7
alexisbarthes0 <abarthesbrisson@gmail.com>
2025-10-10 16:55:42 +0200

    Projet pro
    Meilleur slider projet pro
    ajout image
    Un peu avancé sur le texte de /pro

--- src/app/mes-projets/mes-projets.component.ts
+++ src/app/mes-projets/mes-projets.component.ts
@@ -1,15 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-mes-projets',
   templateUrl: './mes-projets.component.html',
   styleUrls: ['./mes-projets.component.css']
 })
-export class MesProjetsComponent implements OnInit {
+export class MesProjetsComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('scrollWidget', { static: false }) scrollWidget!: ElementRef;
 
-  constructor() { }
+  currentPageIndex = 0;
+  autoScrollInterval: any;
 
-  ngOnInit(): void {
+  pages = [
+    {
+      titre: 'Projet 1',
+      lines: ['Description projet 1...', ''],
+      link: '/',
+      image:'assets/media/background.jpg'
+    },
+    {
+      titre: 'Projet 2',
+      lines: ['Description projet 2...', ''],
+      link: '/',
+      image:'assets/media/background.jpg'
+
+    },
+    {
+      titre: 'Mes Projets',
+      lines: ['Description projet 3...', ''],
+      link: '/',
+      image:'assets/media/background.jpg'
+    }
+  ];
+
+  ngAfterViewInit() {
+    this.scrollToPage(this.currentPageIndex);
+
+// autoscroll totues les 5sec
+    this.autoScrollInterval = setInterval(() => {
+      this.scrollRight();
+    }, 5000);
+  }
+pauseAutoScroll() {
+  clearInterval(this.autoScrollInterval);
+}
+
+resumeAutoScroll() {
+  this.autoScrollInterval = setInterval(() => {
+    this.scrollRight();
+  }, 5000);
+}
+  ngOnDestroy() {
+    // on enlève le timer si le composant est détruit
+    clearInterval(this.autoScrollInterval);
   }
 
+  scrollToPage(index: number) {
+    const container = this.scrollWidget.nativeElement;
+    const page = container.children[index];
+    if (page) {
+      container.scrollTo({
+        left: page.offsetLeft,
+        behavior: 'smooth'
+      });
+    }
+  }
+
+  scrollLeft() {
+    this.currentPageIndex =
+      (this.currentPageIndex - 1 + this.pages.length) % this.pages.length;
+    this.scrollToPage(this.currentPageIndex);
+  }
+
+  scrollRight() {
+    this.currentPageIndex =
+      (this.currentPageIndex + 1) % this.pages.length;
+    this.scrollToPage(this.currentPageIndex);
+  }
 }
