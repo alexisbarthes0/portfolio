0a6812993e6a6eda7383b253afc795d47cc68335
alexisbarthes0 <abarthesbrisson@gmail.com>
2025-10-29 14:50:16 +0100

    Fin pro/sco + contact us
    Fin des pages descripives sur parcours pro et scolaire
    Front de la page contact faite
    Reste à faire: API avec endpoint pour réception message du contact us
    Aller le fun commence maintenant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -13,6 +12,7 @@ import { MentionsLegalesComponent } from './mentions-legales/mentions-legales.co
 import { ContactComponent } from './contact/contact.component';
 import { SituationComponent } from './situation/situation.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -30,7 +30,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
+  contactForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(10)]],
+    });
   }
 
+  get f() {
+    return this.contactForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.contactForm.invalid) {
+      return;
+    }
+
+    console.log('✅ Formulaire envoyé :', this.contactForm.value);
+    alert('Merci pour votre message !');
+    this.contactForm.reset();
+    this.submitted = false;
+  }
 }
